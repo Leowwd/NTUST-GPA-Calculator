1a60f171740703ea0d39354095251c4b7c676425
Leowwd <liaoleo12@gmail.com>
2023-07-12 23:47:15 +0800

    Fourth Commit

--- gpaCalculator.py
+++ gpaCalculator.py
@@ -3,7 +3,6 @@ dic2 = {}
 dic3 = {}
 dic4 = {"GE":0, "FE":0, "廢物國文":0, "PE":0}
 
-
 def fun1(temp):
     temp[4] = abs(int(temp[4]))
     if temp[6] != "不及格":
@@ -28,16 +27,67 @@ def fun3(temp):
         dic2[temp[5]] += 1
         dic3[temp[5]] += temp[4]   
 
+def myPrint():
+    print("-" * 50)
+    print(dic2) #總共幾堂課是啥分數
+    print("-" * 50)
+    print(dic3) #總共幾學分是啥分數
+    print("-" * 50)            
+    print(dic4)
+
+    print("-" * 50)            
+    if dic4["GE"] >= 15:
+        print("You've passed threshold of GE.")
+    else:
+        g = 15 - dic4["GE"]
+        print(f"GE: You need {g} credits.") if g > 1 else print(f"GE: You need {g} credit.")
+    print("-" * 50)
+
+    if dic4["FE"] >= 12:
+        print("You've passed threshold of English.")
+    else:
+        g = 12 - dic4["FE"]
+        print(f"English: You need {g} credits.") if g > 1 else print(f"English: You need {g} credit.")
+    print("-" * 50)
+
+    if dic4["廢物國文"] >= 6:
+        print("You've passed threshold of 廢物國文.")
+    else:
+        g = 6 - dic4["廢物國文"]
+        print(f"廢物國文: You need {g} credits.") if g > 1 else print(f"廢物國文: You need {g} credit.")
+    print("-" * 50)
+
+    if dic4["PE"] == 6:
+        print("You've passed threshold of PE.")
+    else:
+        g = 6 - dic4["PE"]
+        print(f"PE: You need {g} courses.") if g > 1 else print(f"PE: You need {g} course.")
+    print("-" * 50)
+
+def printTotal(ques):
+    if ques == "y":
+        dic1["A+"] = 4
+    elif ques == "n":
+        dic1["A+"] = 4.3
+    total = 0
+    totalCredit = 0
+    for grade, credit in dic3.items():
+        total += dic1[grade] * credit
+        totalCredit += credit
+
+    if ques == "y" or ques == "n":
+        print("-" * 50)
+        print(f"Your gpa is {round(total/totalCredit, 2)}")
+        print("-" * 50)
+
+    if ques == "r":
+        myPrint()
+
 with open("gpa.csv", "r") as putin: #直接把成績查詢那邊複製下來 放到gpa.csv
 
     while True:
         ques = input("Do you want to study abroad? (please enter Y or N or R and enter E to exit)\n").lower()
-        if ques == "y":
-            dic1["A+"] = 4
-        elif ques == "n":
-            dic1["A+"] = 4.3
-        elif ques == "e":
-            break
+        if ques == "e": break
 
         for i in putin.readlines():
             temp = i.split(",")
@@ -48,50 +98,4 @@ with open("gpa.csv", "r") as putin: #直接把成績查詢那邊複製下來 放
             if (temp[2].startswith("CC") and temp[3].startswith("英") and temp[6] != "免修" or temp[2].startswith("FE")) and temp[6] != "不及格":
                 dic4["FE"] += int(temp[4])
 
-        total = 0
-        totalCredit = 0
-        for grade, credit in dic3.items():
-            total += dic1[grade] * credit
-            totalCredit += credit
-
-        if ques == "y" or ques == "n":
-            print("-" * 50)
-            print(f"Your gpa is {round(total/totalCredit, 2)}")
-            print("-" * 50)
-
-        if ques == "r":
-            print("-" * 50)
-            print(dic2) #總共幾堂課是啥分數
-            print("-" * 50)
-            print(dic3) #總共幾學分是啥分數
-            print("-" * 50)            
-            print(dic4)
-
-            print("-" * 50)            
-            if dic4["GE"] >= 15:
-                print("You've passed threshold of GE.")
-            else:
-                g = 15 - dic4["GE"]
-                print(f"GE: You need {g} credits.") if g > 1 else print(f"GE: You need {g} credit.")
-            print("-" * 50)
-
-            if dic4["FE"] >= 12:
-                print("You've passed threshold of English.")
-            else:
-                g = 12 - dic4["FE"]
-                print(f"English: You need {g} credits.") if g > 1 else print(f"English: You need {g} credit.")
-            print("-" * 50)
-
-            if dic4["廢物國文"] >= 6:
-                print("You've passed threshold of 廢物國文.")
-            else:
-                g = 6 - dic4["廢物國文"]
-                print(f"廢物國文: You need {g} credits.") if g > 1 else print(f"廢物國文: You need {g} credit.")
-            print("-" * 50)
-
-            if dic4["PE"] == 6:
-                print("You've passed threshold of PE.")
-            else:
-                g = 6 - dic4["PE"]
-                print(f"PE: You need {g} courses.") if g > 1 else print(f"PE: You need {g} course.")
-            print("-" * 50)
+        printTotal(ques)
